b4136b06d6005f2e2d442de6783bd01ed43a17ec
August Herron <ryderherron@gmail.com>
2025-12-10 21:18:34 +0100

    Concurrent queue added

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
-all: server.c
-	clang -pthread server.c -o server
+all: server.o cqueue.o
+	clang server.o cqueue.o -o server
 
+objfiles: server.c cqueue.c
+	clang -pthread server.c cqueue.c -o server.o cqueue.o
 clean:
-	rm -f server
+	rm -f server server.o cqueue.o
---
+++ cqueue.c
@@ -0,0 +1,87 @@
+#include "pthread.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+// A thread-safe queue
+
+typedef struct _node_t
+{
+  int value;
+  struct _node_t* next;
+} node_t;
+
+typedef struct _cqueue_t
+{
+  node_t* head;
+  node_t* tail;
+  pthread_mutex_t head_lock;
+  pthread_mutex_t tail_lock;
+} cqueue_t;
+
+void cqueue_init(cqueue_t* cq)
+{
+  // create a dummy sentinel node in order to
+  // separate enqueue and dequeue operations;
+  // head and tail never point to the same node
+  // when the queue is non-empty to prevent race
+  // conditions when enqueuing and dequeuing simultaneously
+  node_t* sentinel = (node_t*)malloc(sizeof(node_t));
+  if (sentinel == NULL)
+  {
+    fprintf(stderr, "Failed to initialize cqueue.\n");
+    exit(1);
+  }
+  sentinel->value = 0;
+  sentinel->next = NULL;
+
+  cq->head = sentinel;
+  cq->tail = sentinel;
+
+  pthread_mutex_init(&cq->head_lock, NULL);
+  pthread_mutex_init(&cq->tail_lock, NULL);
+}
+
+void cqueue_enqueue(cqueue_t* cq, int value)
+{
+  node_t* new_node = (node_t*)malloc(sizeof(node_t));
+  if (new_node == NULL)
+  {
+    fprintf(stderr, "Out of memory (cqueue).\n");
+    exit(1);
+  }
+  new_node->value = value;
+  new_node->next = NULL;
+
+  pthread_mutex_lock(&cq->tail_lock);
+  cq->tail->next = new_node;
+  cq->tail = new_node;
+  pthread_mutex_unlock(&cq->tail_lock);
+}
+
+int cqueue_dequeue(cqueue_t* cq, int* ret_value)
+{
+  pthread_mutex_lock(&cq->head_lock);
+  node_t* sentinel = cq->head;
+  if (sentinel->next == NULL)
+  {
+    pthread_mutex_unlock(&cq->head_lock);
+    return -1; // queue is empty
+  }
+
+  *ret_value = sentinel->next->value;
+  cq->head = sentinel->next;
+
+  free(sentinel);
+  pthread_mutex_unlock(&cq->head_lock);
+  return 0;
+}
+
+void cqueue_free(cqueue_t* cq)
+{
+  int dummy;
+  while (cqueue_dequeue(cq, &dummy) != -1)
+    ;
+  free(cq->head);
+  pthread_mutex_destroy(&cq->head_lock);
+  pthread_mutex_destroy(&cq->tail_lock);
+}
